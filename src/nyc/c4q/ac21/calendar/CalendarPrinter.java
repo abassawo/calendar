b0a15695faf588631061b4409c1e75bc34d96c25
abassawo <abass.bayo@gmail.com>
2015-04-04 18:00:58 -0400

    Changes made to calendar printint function

--- src/nyc/c4q/ac21/calendar/CalendarPrinter.java
+++ src/nyc/c4q/ac21/calendar/CalendarPrinter.java
@@ -4,8 +4,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.HashSet;
-public class CalendarPrinter
-{
+
+public class CalendarPrinter {
+
     /**
      * Prints a calendar for a month.
      * <p/>
@@ -24,8 +25,8 @@ public class CalendarPrinter
      *
      * @param date The date containing the month to print.
      */
-    public static void printMonthCalendar(Calendar date)
-    {
+    public static void printMonthCalendar(Calendar date) {
+
         // FIXME: Write this.
         // Use these methods to help you:
         //System.out.println(DateTools.getMonthNames());
@@ -39,113 +40,68 @@ public class CalendarPrinter
         //int workDay_ = date.get(Calendar.DAY_OF_WEEK);
         //int monthday1 = date.getFirstDayOfWeek();
         int today = date.get(Calendar.DAY_OF_MONTH);
-        Calendar newCal =  Calendar.getInstance();
-        newCal.set(year, month, 1);
+        Calendar newCal = Calendar.getInstance();
 
-        ArrayList<Integer>cal = new ArrayList<Integer>();
+        newCal.set(year, month, 1);  //stores their date. 1st day of said month
 
-//        HashSet<Integer>sun = new HashSet<Integer>();
-//        HashSet<Integer>mon = new HashSet<Integer>();
-//        HashSet<Integer>tue = new HashSet<Integer>();
-//        HashSet<Integer>wed = new HashSet<Integer>();
-//        HashSet<Integer>thur = new HashSet<Integer>();
-//        HashSet<Integer>fri = new HashSet<Integer>();
-//        HashSet<Integer>sat = new HashSet<Integer>();
-//
-//        if(today == date.get(Calendar.SUNDAY))
-//            sun.add(today);
-//        else if(today == date.get(Calendar.MONDAY))
-//            mon.add(today);
-//        else if(today == date.get(Calendar.TUESDAY))
-//        tue.add(today);
-//        else if(today == date.get(Calendar.WEDNESDAY))
-//         wed.add(today);
-//        else if(today == date.get(Calendar.THURSDAY))
-//        thur.add(today);
-//        else if(today == date.get(Calendar.FRIDAY))
-//        fri.add(today);
 
-        int offset = newCal.get(Calendar.DAY_OF_WEEK);
-        int endofMonth = newCal.getActualMaximum(Calendar.DAY_OF_MONTH);
-        int numofDaysinMonth = endofMonth - newCal.getFirstDayOfWeek();
+        int offset = newCal.get(Calendar.DAY_OF_WEEK); //What day of the week is day 1?
+        int offsetEnd = 7 - offset; //fixme
+
+        int endofMonth = newCal.getActualMaximum(Calendar.DAY_OF_MONTH); //Length of specified month.
+
 
         //int offsetdMonth = newCal.get(Calendar.DAY_OF_WEEK_IN_MONTH);
         System.out.println("first day of this month is " + offset);
-        System.out.println("last day of month is " + endofMonth);
-
+        System.out.println("last day of month is " + endofMonth + "\n");
 
         //System.out.println(13 % 7);
 
-       // System.out.println(offset); //number of days for week to start. offset of 1 represents sunday
+        // System.out.println(offset); //number of days for week to start. offset of 1 represents sunday
         //2 is Monday. 3 is tue. 4 is wed. 5 is thurs. 6 is fri. sat is sunday
         String offsetSpace = "";
-        for (int i = 0; i < offset; i++){
-            offsetSpace+= " ";
-        }
+//        for (int i = 0; i <= endofMonth; i++) {
+//            offsetSpace += " " ;
+//            calPrint +=
+//        }
 
 
         //explore modular division. EX: IF Today is a saturday, that is the 6th day of week.
         // 7 mod 6 = 1 meaning there is only space for 1 calendar left after this.2
         int MaxDay = newCal.getActualMaximum(Calendar.DAY_OF_WEEK);
-        //System.out.print(offsetSpace + today + " * ");
-            for (int i = 1; i <= today; i++) {
-                int currentDay = i;
-                String calPrint = "  ";
-                calPrint += i;
 
-               if (i == 1) {
-                   if (offset == 1) {
-                       calPrint = "  " + i;
-                   } else if (offset == 2) {
-                       calPrint = "    " + i;
-                   } else if (offset == 3) {
-                       calPrint = "     " + i;
-                   } else if (offset == 4) {
-                       calPrint = "        " + i;
-                   } else if (offset == 5) {
-                       calPrint = "          " + i;
-                   } else if (offset == 5) {
-                       calPrint = "             " + i;
-                   } else {
-                       calPrint = "               " + i;
-                   }
+        for (int i = 1; i <= date.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+            String calPrint = " ";
 
-               }
-                if (i == today){
-                    calPrint += "*";
+
+            if(offset > 1) {
+                for (offset = 0; offset < offsetEnd + 1; offset++) {
+                    offsetSpace += "  ";
                 }
-                if ((i > 1) && (i % 7) == 1) {
-                    calPrint += "\n";
-                }
-                System.out.print(calPrint);
-
-
             }
 
-//        for (int x = 8 - offset; x <= offset + 7; x++) {
-//            String calPrintthirdLine = offsetSpace;
-//            calPrintthirdLine += x;
-//            System.out.println(calPrintthirdLine);
-//        }w
-        //}
 
-
-        //String calPrint = offsetSpace;
-//        for (int i = today + 1; i <= date.getActualMaximum(Calendar.DAY_OF_MONTH); i++){
-//
-//            String calPrint = offsetSpace + i;
-//
-//        }
-
-
-       // find beginning of week or month and what day it is
-        //System.out.println(date.getMinimalDaysInFirstWeek());
-            for(int i = today + 1; i <= date.getActualMaximum(Calendar.DAY_OF_MONTH); i++)
-            {
-                String calPrint = " ";
-                calPrint += i;
-                System.out.print(calPrint);
+            if(i == 1){     //
+                calPrint += offsetSpace;
             }
+
+            if (i < 10) {       //this is to determine which texts need space
+                calPrint += " ";
+            } else if (i > 10) {
+                calPrint += "";
+            }
+
+            calPrint += i;
+
+            if (i == today) {
+                calPrint += "*";
+            }
+            if ((i > 1) && (i % 7) == (offset % 7)) {   //this is to regulate when we jump to next line. needs fix.
+                calPrint += "\n";
+            }
+
+            System.out.print(calPrint);
+        }
 
     }
 
